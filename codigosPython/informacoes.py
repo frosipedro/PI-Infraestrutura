32f208cda74dffb5f1b799e87919a4235b311e9d
Pedro Rockenbach Frosi <107145213+frosipedro@users.noreply.github.com>
2024-01-09 01:23:18 -0300

    Add files via upload

---
+++ codigosPython/informacoes.py
@@ -0,0 +1,15 @@
+# Constantes de Configuracao MQTT
+MQTT_BROKER_ADDRESS = "10.41.20.15"
+MQTT_BROKER_PORT = 1996
+MQTT_BROKER_USERNAME = "piinfra"
+MQTT_BROKER_PASSWORD = "piinfra"
+MQTT_TOPIC = 'gateway'
+
+# Configuracoes do email
+email_from = "testePI666@gmail.com"
+email_password = "eqii mgae gtme aaen"
+email_to = "pedrorfrosi@gmail.com"
+email_subject = "Alerta de Problema no Gateway MQTT"
+email_message = "O gateway com as seguintes informações: "
+email_message2 = " está há "
+email_message3 = " horas sem enviar mensagens."
